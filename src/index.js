98c664be2bcd501868e1b79ff9998b51e398eaa6
SUYASH <jsuyash864@gmail.com>
2025-04-07 16:40:37 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -3,21 +3,27 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email-service');
+// const { sendBasicEmail } = require('./services/email-service');
+const TicketController = require('./controllers/ticket-controller');
+
+const jobs = require('./utils/job');
 
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.post('/api/v1/tickets', TicketController.create); 
+
     app.listen(PORT, () => {
         console.log(`Started server on port: ${PORT}`);
-
-        sendBasicEmail(
-            'support@admin.com',
-            'js96025222@gmail.com',
-            'This is a testing email',
-            'Hey, how are you? I hope you like the support.');
+        jobs();
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'js96025222@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you? I hope you like the support.'
+        // );
     });
 }
 
---
+++ src/utils/job.js
@@ -0,0 +1,31 @@
+const cron = require('node-cron');
+const emailService = require('../services/email-service');
+/**
+ * 10:00 am
+ * Every 5 minutes
+ * We will check are their any pending emails which was expected to be sent
+ * By now and is prep
+ */
+
+const setupJobs = () => {
+    cron.schedule('*/2 * * * *', async () => {
+        const response = await emailService.fetchPendingEmails();
+        response.forEach((email) => {
+            sender.sendMail ({
+                to: email.recipientEmail,
+                subject: email.subject,
+                text: email.content
+            },async (err, data) => {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log(data);
+                    await emailService.updateTicket(email.id, {status: "SUCCESS"});
+                }
+            });
+        })
+        console.log(response);
+    });
+}
+
+module.exports = setupJobs;
